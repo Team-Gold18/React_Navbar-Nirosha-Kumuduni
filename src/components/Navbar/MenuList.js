18c3cdd9c6db047eee7e67d97d62049a845b2c2e
KUMUDUNI98 <niroshakumuduni1998@gmail.com>
2022-02-03 10:57:11 +0530

    initial commit

---
+++ src/components/Navbar/MenuList.js
@@ -0,0 +1,23 @@
+//route 
+export const MenuList = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "Key Features",
+    url: "/features",
+  },
+  {
+    title: "Pricing",
+    url: "/pricing",
+  },
+  {
+    title: "Testimonial",
+    url: "/testimonials",
+  },
+  {
+    title: "Try For Free",
+    url: "/demo",
+  },
+];
